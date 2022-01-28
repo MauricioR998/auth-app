9a7f5a04b136ef3dbcbddfa032fbc25f810355fa
MauricioR998 <mauricio.rivas98@gmail.com>
2022-01-28 13:46:07 -0500

    Passport JWT integrated

--- .build/src/users/users.service.js
+++ .build/src/users/users.service.js
@@ -6,20 +6,29 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppModule = void 0;
+exports.UsersService = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
-const auth_module_1 = require("./auth/auth.module");
-const users_module_1 = require("./users/users.module");
-let AppModule = class AppModule {
+let UsersService = class UsersService {
+    constructor() {
+        this.users = [
+            {
+                userId: 1,
+                username: 'john',
+                password: 'changeme',
+            },
+            {
+                userId: 2,
+                username: 'maria',
+                password: 'guess',
+            },
+        ];
+    }
+    async findOne(username) {
+        return this.users.find((user) => user.username === username);
+    }
 };
-AppModule = __decorate([
-    (0, common_1.Module)({
-        imports: [auth_module_1.AuthModule, users_module_1.UsersModule],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
-    })
-], AppModule);
-exports.AppModule = AppModule;
-//# sourceMappingURL=app.module.js.map
+UsersService = __decorate([
+    (0, common_1.Injectable)()
+], UsersService);
+exports.UsersService = UsersService;
+//# sourceMappingURL=users.service.js.map
